d07f63ad84de39f46999453c136463df7c302c2f
Feppoo <167021596+Feppoo@users.noreply.github.com>
2026-05-12 13:13:45 -0600

    Update
    Solo falta control

--- src/logica/Cliente.java
+++ src/logica/Cliente.java
@@ -1,15 +1,19 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
-	private 
+	private List<Orden> ordenes = new ArrayList<>();
 	private String id;
 	private String nombre;
 	private String email;
 	
-	public Cliente(String id, String nombre, String email) {
+	public Cliente(String id, String nombre, String email, List<Orden> ordenes) {
 		this.id = id;
 		this.nombre = nombre;
 		this.email = email;
+		this.ordenes = ordenes;
 	}
 
 	public String getId() {
@@ -36,5 +40,15 @@ public class Cliente {
 		this.email = email;
 	}
 	
+	public List<Orden> getListaOrdenes() {
+		return ordenes;
+	}
 	
+	public boolean addOrden(Orden orden) {
+		return ordenes.add(orden);
+	}
+	
+	public boolean delOrden(Orden orden) {
+		return ordenes.remove(orden);
+	}
 }
--- src/logica/Orden.java
+++ src/logica/Orden.java
@@ -1,17 +1,29 @@
 package logica;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Orden {
+	private List<Linea> lineas = new ArrayList<>();
+	private Cliente cliente;
 	private static int consecutivo = 0;
 	private int numero;
 	private LocalDate fecha;
 	private Estado estado;
 	
-	public Orden(Estado estado) {
+	public Orden(Cliente cliente, Estado estado, List<Linea> lineas) {
 		numero = ++consecutivo;
 		fecha = LocalDate.now();
 		this.estado = estado;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
 	}
 
 	public int getNumero() {
@@ -34,4 +46,15 @@ public class Orden {
 		return fecha;
 	}
 	
+	public List<Linea> getLineas() {
+		return lineas;
+	}
+	
+	public boolean addLinea(Linea linea) {
+		return lineas.add(linea);
+	}
+	
+	public boolean delLinea(Linea linea) {
+		return lineas.remove(linea);
+	}
 }
